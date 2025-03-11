70fae89f654e3b3241692a9c59538fe7fbdd601a
Dhanush <legendarycoder6@gmail.com>
2025-03-11 17:58:27 +0530

    some pattern printing files created

---
+++ C++ core/Pattern printing/Aqua10.cpp
@@ -0,0 +1,39 @@
+#include<iostream>//Program for printing Star bridges pattern
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter the number of lines: ";
+    cin >> n;//user input of n number of lines
+
+//       *
+//      *0*
+//     *000*
+//    *00000*
+//   *0000000*
+//  *000000000*
+// *************
+
+    int nst = 1;
+    int nsp = n-1;
+   for(int i=1;i<=n-1;i++){//this loop prints lines (n-1)is because last line we are printing separately
+    cout<<endl;
+    for(int k=1;k<=nsp;k++){//this loop prints space before pattern
+        cout<<" ";
+    }
+    for(int j=1;j<=nst;j++){//this is similar loop as pyramid pattern's loop
+        if(j==1 || j==nst){//only outside stars will be printed
+            cout<<"*";
+        }
+        else{
+            cout<<0;//inside 0 will be printed
+        }
+    }    
+    nst+=2;//stars are increamented by 2
+    nsp--;//spaces are decreamented by 1
+}
+  cout<<endl;//it is mendatory
+for(int i=1;i<=2*n-1;i++){//this loop prints last line stars
+    cout<<"*";
+}
+    return 0;
+}
---
+++ C++ core/Pattern printing/Aqua9.cpp
@@ -0,0 +1,33 @@
+#include<iostream>//Program for printing Star bridges pattern
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter the number of lines: ";
+    cin >> n;//user input of n number of lines
+
+    // 1   10 9 8 7 6 5 4 3 2 1
+    // 1 2   10 9 8 7 6 5 4 3 2
+    // 1 2 3   10 9 8 7 6 5 4 3
+    // 1 2 3 4   10 9 8 7 6 5 4 
+    // 1 2 3 4 5   10 9 8 7 6 5
+    // 1 2 3 4 5 6   10 9 8 7 6
+    // 1 2 3 4 5 6 7   10 9 8 7
+    // 1 2 3 4 5 6 7 8   10 9 8
+    // 1 2 3 4 5 6 7 8 9   10 9
+    // 1 2 3 4 5 6 7 8 9 10   10  
+
+    
+   for(int i=1;i<=n;i++){//this loop prints is for printing number of lines
+    cout<<endl;
+    for(int j=1;j<=i;j++){//this loop is for printing number increasing order
+        cout<<j<<" ";
+    }
+    for(int k=1;k<=2;k++){//this loop is for printing spaces in between 2 patterns
+        cout<<"  ";
+    }
+    for(int j=n;j>=i;j--){//this loop is for printing number decreasing order
+        cout<<j<<" ";
+    }
+}
+    return 0;
+}
