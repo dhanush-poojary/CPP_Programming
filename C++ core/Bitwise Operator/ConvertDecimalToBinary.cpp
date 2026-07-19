dab273b84fc1f19bac3195cdd354a589ea55554f
Dhanush <legendarycoder6@gmail.com>
2026-07-19 15:38:39 +0530

    Updated BitWise Operator File

--- C++ core/Bitwise Operator/ConvertDecimalToBinary.cpp
+++ C++ core/Bitwise Operator/ConvertDecimalToBinary.cpp
@@ -28,13 +28,13 @@ int main(){
  //cout<<binary_to_decimal(str); //Binary to decimal using left shit operator bitwise operator
 
 
- string str = "";  //Decimal to binary 
- int num = 18;
-  while(num!=0){//here we are taking the lcm of that number 
-    int n = num/2;//dividing 
-    str+=(to_string(num%2));//storing remainder
-    num = n;//reducing number by lcm
-  }
-  reverse(str.begin(),str.end());//as we need to store the remainders from bottom to top reverse it
-  cout<<str;
+//  string str = "";  //Decimal to binary 
+//  int num = 18;
+//   while(num!=0){//here we are taking the lcm of that number 
+//     int n = num/2;//dividing 
+//     str+=(to_string(num%2));//storing remainder
+//     num = n;//reducing number by lcm
+//   }
+//   reverse(str.begin(),str.end());//as we need to store the remainders from bottom to top reverse it
+//   cout<<str;
 }
